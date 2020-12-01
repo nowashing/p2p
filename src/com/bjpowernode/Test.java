f9d008287d80aad97116e7243e958698b2bd69cb
nowashing <24040486@qq.com>
2020-12-01 10:31:35 +0800

    crm立项

---
+++ src/com/bjpowernode/Test.java
@@ -0,0 +1,17 @@
+package com.bjpowernode;
+/**
+ * @ClassName Test
+ * @Description 一句话描述此类或方法的作用
+ * @author PowerNode
+ * @Date 2020/12/1 10:06
+ * @version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+
+    }
+}
